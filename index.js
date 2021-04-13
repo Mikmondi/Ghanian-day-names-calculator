29d4fdd704d66b10642f7be18d62cd21f2f99e89
Mikmondi <mikmondi@gmail.com>
2021-04-13 06:17:25 +0300

    added javascript file to display the day of birth

---
+++ index.js
@@ -0,0 +1,15 @@
+function getNameFromDate(dateBorn,gender){
+    var dateOfBirth = new Date(dateBorn);
+    var dayOfTheWeek = dateOfBirth.getDay();
+    var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+    var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+    if(gender == "male"){
+        document.getElementById(displayName).innerHTML = "Your Akan Name is " + maleNames[dayOfTheWeek];
+    }
+    else if(gender == "female"){
+        document.getElementById(displayName).innerHTML = "Your Akan Name is " + femaleName[dayOfTheWeek];
+    }
+    else{
+        document.getElementById(displayName).innerHTML = "Error check that you've entered your details correctly";
+    }
+}
