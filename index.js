d09fa77968c6fd91be61b4e1616233b6e0fcd298
Mikmondi <mikmondi@gmail.com>
2021-04-13 07:57:35 +0300

    added paragragh to display results

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function getNameFromDate(dateBorn,gender){
-    var dateOfBirth = new Date(dateBorn);
+    var dateOfBirth = new Date(dateString);
     var dayOfTheWeek = dateOfBirth.getDay();
     var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
     var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
@@ -10,12 +10,12 @@ function getNameFromDate(dateBorn,gender){
         document.getElementById(displayName).innerHTML = "Your Akan Name is " + femaleName[dayOfTheWeek];
     }
     else{
-        document.getElementById(displayName).innerHTML = "Error check that you've entered your details correctly";
+        "Error Check details entered!";
     }
 }
 
 function getDayBorn(){
-    var dateBorn = prompt("Please enter your date of birth:mm/dd/yy");
+    var dateString = prompt("Please enter your date of birth:mm/dd/yyyy");
     var gender = prompt("What is your Gender, female or male?");
     getNameFromDate(dateBorn,gender);
 }
